10ecf5f4b5594219aec9f3ed9929a5004e58eae8
rampreetha <53893791+rampreetha@users.noreply.github.com>
2019-08-14 15:53:52 +0530

    Update space.py

--- space.py
+++ space.py
@@ -1,12 +1,12 @@
 fname = input("Enter file name: ")
-k = 0
+p = 0
  
-with open(fname, 'r') as f:
+with open(fname, 'u') as f:
     for line in f:
         words = line.split()
         for i in words:
             for letter in i:
                 if(letter.isspace):
-                    k=k+1
+                    p=p+1
 print("Occurrences of blank spaces:")
-print(k)
+print(p)
