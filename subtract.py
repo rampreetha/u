96016aeb070d70b53d70062da6aee97d3b9ab3d2
rampreetha <53893791+rampreetha@users.noreply.github.com>
2019-08-14 15:44:18 +0530

    Update subtract.py

--- subtract.py
+++ subtract.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-s1 = '10:04:00'
-s2 = '11:03:11' # for example
+p1 = '10:04:00'
+p2 = '11:03' # 
 format = '%H:%M:%S'
-time = datetime.strptime(s2, format) - datetime.strptime(s1, format)
+time = datetime.strptime(p2, format) - datetime.strptime(p1, format)
 print time
